6567364990420386156e6537afd45b5b0b44c3f8
Fredabod <fredrickbolutife@gmail.com>
2026-05-14 19:53:41 +0100

    updated code to mvc architecture

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbUrl = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbUrl);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
